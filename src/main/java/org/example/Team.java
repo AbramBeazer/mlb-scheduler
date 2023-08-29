a50e86d83371f856bf1b685114b20daa74ee5720
abrambeazer <abrambeazer@gmail.com>
2023-08-29 16:08:51 -0700

    Added abbreviations for each team and for their designated interleague rivals.

--- src/main/java/org/example/Team.java
+++ src/main/java/org/example/Team.java
@@ -1,10 +1,16 @@
 package org.example;
 
+import java.time.LocalDate;
 import java.util.Objects;
 
 public class Team {
+
     private String location;
     private String name;
+    private TeamCode code;
+    private TeamCode rival;
+
+    private LocalDate mostRecentGame;
 
     public String getLocation() {
         return location;
@@ -20,6 +26,30 @@ public class Team {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public TeamCode getCode() {
+        return code;
+    }
+
+    public void setCode(TeamCode code) {
+        this.code = code;
+    }
+
+    public TeamCode getRival() {
+        return rival;
+    }
+
+    public void setRival(TeamCode rival) {
+        this.rival = rival;
+    }
+
+    public LocalDate getMostRecentGame() {
+        return mostRecentGame;
+    }
+
+    public void setMostRecentGame(LocalDate mostRecentGame) {
+        this.mostRecentGame = mostRecentGame;
     }
 
     @Override
---
+++ src/main/java/org/example/TeamCode.java
@@ -0,0 +1,49 @@
+package org.example;
+
+public enum TeamCode {
+    ATL,
+    PHI,
+    MIA,
+    NYM,
+    WSH,
+    PIT,
+    CHC,
+    MIL,
+    STL,
+    CIN,
+    LAD,
+    SD,
+    SF,
+    COL,
+    ARI,
+    SEA,
+    OAK,
+    LAA,
+    TEX,
+    HOU,
+    KC,
+    CHW,
+    MIN,
+    DET,
+    CLE,
+    NYY,
+    BOS,
+    TOR,
+    BAL,
+    TB;
+
+    //    private final String value;
+    //
+    //    TeamCode(String value) {
+    //        this.value = value;
+    //    }
+    //
+    //    public String getValue() {
+    //        return value;
+    //    }
+    //
+    //    @Override
+    //    public String toString() {
+    //        return getValue();
+    //    }
+}
